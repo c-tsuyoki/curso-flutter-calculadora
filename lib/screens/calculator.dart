029c0c90618a71571820555f01cac1bf2dc0f4f9
unknown <cristian175.contato@gmail.com>
2021-03-30 11:02:20 -0300

    Implementing graphical interface

---
+++ lib/screens/calculator.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../components/display.dart';
+import '../components/keyboard.dart';
+import '../models/memory.dart';
+
+class Calculator extends StatefulWidget {
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+
+  _onPressed(String command) {
+    setState(() {
+      memory.applyCommand(command);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Column(
+        children: <Widget>[
+          Display(memory.value),
+          Keyboard(_onPressed),
+        ],
+      ),
+    );
+  }
+}
